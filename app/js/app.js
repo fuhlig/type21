9763116647e6bee75232b4d76be291c0243041e8
Florian Uhlig <fuhlig@stud.hs-bremen.de>
2012-11-09 01:19:24 +0000

    * updated (dummy) data * updated routing + course-list + coure-detail

--- app/js/app.js
+++ app/js/app.js
@@ -5,8 +5,8 @@
 angular.module('type21', []).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
-      when('/courses', {templateUrl: 'partials/course-list.html',   controller: CourseListCtrl}).
-      when('/courses/:courseId', {templateUrl: 'partials/course-detail.html',   controller: CourseDetailCtrl}).
-      when('/lesson/:lessonId', {templateUrl: 'partials/lesson-list.html', controller: LessonCtrl}).
+      when('/courses', {templateUrl: 'views/course-list.html',   controller: CourseListCtrl}).
+      when('/courses/:courseId', {templateUrl: 'views/course-detail.html',   controller: CourseDetailCtrl}).
+      when('/:courseId/:lessonId', {templateUrl: 'views/lesson-list.html', controller: LessonCtrl}).
       otherwise({redirectTo: '/courses'});
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -14,6 +14,7 @@ function CourseListCtrl($scope, $http) {
 	      // "tipLocation": "top"
 	      // "timer": 2000
 	    });
+	    console.log("tour start!");
   	}
 }
 
@@ -25,13 +26,29 @@ function CourseDetailCtrl($scope, $routeParams, $http) {
 	$http.get("data/courses/" + $routeParams.courseId +".json").success(function(data) {
 		$scope.course = data;
 	});
+	$scope.$routeParams = $routeParams;
 }
 
 function LessonCtrl($scope, $routeParams, $http) {
+	// get course list	
 	$http.get("data/courses.json").success(function(data) {
 		$scope.courses = data;
 	});
 
+	// get course details
+	$http.get("data/courses/" + $routeParams.courseId +".json").success(function(data) {
+		$scope.course = data;
+	});
+
+	// get lesson details
 	$http.get("data/lessons/" + $routeParams.lessonId +".json").success(function(data) {
 		$scope.lesson = data;
-	});}
+		$scope.content = data.features[0].content;
+	});
+
+	$scope.setContent = function(content) {
+		$scope.content = content;
+	}
+
+	$scope.$routeParams = $routeParams;
+}
