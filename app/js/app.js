a15959fa1a0e14547343a1abd2a0193100a84ea0
Florian Uhlig <fuhlig@stud.hs-bremen.de>
2012-11-30 19:43:21 +0000

    markdown test

--- app/js/app.js
+++ app/js/app.js
@@ -2,11 +2,23 @@
 
 /* App Module */
 
-angular.module('type21', []).
+angular.module('type21', ['mdConverter']).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
       when('/courses', {templateUrl: 'views/course-list.html',   controller: CourseListCtrl}).
       when('/courses/:courseId', {templateUrl: 'views/course-detail.html',   controller: CourseDetailCtrl}).
       when('/:courseId/:lessonId', {templateUrl: 'views/lesson-list.html', controller: LessonCtrl}).
       otherwise({redirectTo: '/courses'});
-}]);
+// }]);
+
+  }])
+  .directive('markdown', function() {
+    var converter = new Showdown.converter();
+    return {
+        restrict: 'E',
+        link: function(scope, element, attrs) {
+            var htmlText = converter.makeHtml(element.text());
+            element.html(htmlText);
+        }
+    }	
+});
--- app/js/directives.js
+++ app/js/directives.js
@@ -1,3 +1,21 @@
 'use strict';
 
 /* Directives */
+
+
+/* mdConverter
+ * CUSTOM COMPONENT TO CONVERT MARKDOWN TO HTML VIA SHOWDOWN.JS
+ * based on John Linkquist's "AngularJS Custom Components"
+ */
+
+angular.module('mdConverter', [])
+    .directive('markdown', function () {
+        var converter = new Showdown.converter();
+        return{
+            restrict:'E',
+            link:function(scope, element, attrs) {
+	            var htmlText = converter.makeHtml(element.text());
+	            element.html(htmlText);
+            }
+        }
+    });
