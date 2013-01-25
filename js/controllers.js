433502d30394ca46a69ec137477dbf711b7e2d14
Florian Uhlig <fuhlig@stud.hs-bremen.de>
2013-01-25 15:28:02 +0100

    fixed choosing-type lesson

--- js/controllers.js
+++ js/controllers.js
@@ -6,15 +6,6 @@ function CourseListCtrl($scope, $http) {
 	$http.get("data/courses.json").success(function(data) {
 		$scope.courses = data;
 	});
-
-	// $scope.startTour = function() {
-	//     $("#introTour").joyride({
-	//       // Options
-	//       // "tipLocation": "top"
-	//       // "timer": 2000
-	//       "nextButton": false
-	//     });
- //  	}
 }
 
 function CourseDetailCtrl($scope, $routeParams, $http) {
@@ -44,12 +35,6 @@ function LessonCtrl($scope, $routeParams, $http, $location) {
 	$http.get("data/lessons/" + $routeParams.lessonId +".json").success(function(data) {
 		$scope.lesson = data;
 		$scope.content = data.features[0].src;
-
-		// $scope.resources = data.features[3].resources;
-		// if (data.features.id == "resources") {
-		// 	$scope.resources = data.features.resources;
-		// 	console.log("init resources");
-		// }
 	});
 
 	// get quiz data
@@ -61,21 +46,8 @@ function LessonCtrl($scope, $routeParams, $http, $location) {
 	// set content according to data
 	$scope.setContent = function(content) {
 		$scope.content = content;
-		// if (data.features.id == "resources") {
-		// 	console.log("resources");
-		// } 
-		// else {
-		// 	console.log("other");
-		// };
 	}
 
 	$scope.$routeParams = $routeParams;
 	$scope.$location = $location;
 }
-
-// function QuizControl($scope, $http) {
-// 	$http.get("data/quizzes.json").success(function(quizData) {
-// 		$scope.quiz = quizData;
-// 	});
-// }
-
